1b3694ab6f6669024724ebbb3da8a725947feb71
rwgeaston <rwgeaston@gmail.com>
2016-06-26 11:18:18 +0100

    several more rules to get 25x25 puzzle working

--- nonogram_solver.py
+++ nonogram_solver.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
+import itertools
 
-from nonograms import NonogramGrid, empty, empty_tile
+from nonograms import NonogramGrid, empty, empty_tile, NonogramImpossible
 
 
 def generate_blocks(values, empty_at_start=0, separator=empty):
@@ -11,9 +12,11 @@ def generate_blocks(values, empty_at_start=0, separator=empty):
             yield value
         yield separator
 
+
 def normal(whatever):
     # for syntactic reasons i want the null function later
     return whatever
+
 
 # decorator that takes a rule aimed at one row or column and does the whole lot
 # most rules will need this
@@ -31,11 +34,12 @@ def try_every_row_and_column(rule_function):
 
 class NonogramSolver(NonogramGrid):
     rules = [
-        'fill_fully', 'fill_middle', 'cross_out_too_far_from_any_block',
+        'fill_fully_entire_line', 'fill_middle', 'cross_out_too_far_from_any_block',
         'got_enough_filled_or_not_filled', 'fill_block_if_it_touches_edge',
         'rule_out_values_too_small_for_this_block', 'rule_out_values_based_on_already_used_up',
         'next_to_known_empty', 'remove_options_if_other_pieces_before_it',
-        'cross_out_too_far_from_known_value', 'block_long_enough'
+        'cross_out_too_far_from_known_value', 'block_long_enough', 'split_row_by_known_block',
+        'fill_from_edge', 'too_far_from_known_block_repeated_values', 'eliminate_wrong_side'
     ]
 
     def try_all_rules(self):
@@ -48,11 +52,14 @@ class NonogramSolver(NonogramGrid):
     # if the values for one row + the number of values - 1 is equal to the length of the row,
     # you can fill it in fully. crossed out positions at either end can be subtracted from target sum.
     @try_every_row_and_column
-    def fill_fully(self, index, values, direction):
+    def fill_fully_entire_line(self, index, values, direction):
         tiles = self.get_line(direction, index)
+        return self.fill_fully(values, direction, tiles)
+
+    def fill_fully(self, values, direction, tiles):
         empty_at_start, empty_at_end = self.get_empty_count_at_ends(tiles, direction)
         if sum(values) + len(values) - 1 == self.size[direction] - empty_at_start - empty_at_end:
-            for tile, value in zip(self.get_line(direction, index), generate_blocks(values, empty_at_start)):
+            for tile, value in zip(tiles, generate_blocks(values, empty_at_start)):
                 tile.set_only_option(value, direction)
             return True
         else:
@@ -129,7 +136,6 @@ class NonogramSolver(NonogramGrid):
                 spaces.append((start, length))
         return spaces
 
-
     @try_every_row_and_column
     def cross_out_too_far_from_any_block(self, index, values, direction):
         tiles = self.get_line(direction, index)
@@ -200,13 +206,14 @@ class NonogramSolver(NonogramGrid):
 
     def get_empty_count_at_ends(self, tiles, direction):
         empty_at_start = 0
-        while tiles[empty_at_start].decided[direction] and not tiles[empty_at_start].filled:
+        while empty_at_start < len(tiles) and tiles[empty_at_start].decided[direction] and not tiles[empty_at_start].filled:
             empty_at_start += 1
         empty_at_end = 0
-        while tiles[-empty_at_end-1].decided[direction] and not tiles[-empty_at_end-1].filled:
+        while empty_at_end < len(tiles) and tiles[-empty_at_end-1].decided[direction] and not tiles[-empty_at_end-1].filled:
             empty_at_end += 1
         return empty_at_start, empty_at_end
 
+    # This should be deprecated based on fill_from_edge being strictly better?
     @try_every_row_and_column
     def fill_block_if_it_touches_edge(self, index, values, direction):
         # If the first or last tile is filled, we know the position of the entire first or last block
@@ -223,6 +230,23 @@ class NonogramSolver(NonogramGrid):
                 changes_made += tiles[-1-position].set_only_option(values[-1], direction)
             changes_made += tiles[-1-values[-1]].set_only_option(empty)
 
+        return changes_made
+
+    @try_every_row_and_column
+    def fill_from_edge(self, index, values, direction):
+        # Starting from an edge, as long as the tiles are filled or decided, we can make the filled ones decided.
+        tiles = self.get_line(direction, index)
+        changes_made = False
+
+        for search_direction in [normal, reversed]:
+            values_iterator = itertools.chain(*[[value for _ in xrange(value)] for value in search_direction(values)])
+            for tile in search_direction(tiles):
+                if not (tile.filled or tile.decided[direction]):
+                    # That's as far as we can be certain from this edge
+                    break
+                if tile.filled:
+                    next_value = values_iterator.next()
+                    changes_made += tile.set_only_option(next_value, direction)
         return changes_made
 
     @try_every_row_and_column
@@ -262,6 +286,9 @@ class NonogramSolver(NonogramGrid):
     def rule_out_values_based_on_already_used_up(self, index, values, direction):
         # Is this really an extension of 'got_enough_filled_or_not_filled'?
         tiles = self.get_line(direction, index)
+        return self.rule_out_values_based_on_already_used_up_partial(index, values, direction, tiles)
+
+    def rule_out_values_based_on_already_used_up_partial(self, index, values, direction, tiles):
         observed_counts = defaultdict(int)
 
         for tile in tiles:
@@ -372,13 +399,12 @@ class NonogramSolver(NonogramGrid):
                 tile_might_change.remove_option(value, direction)
         return changes_made
 
-    @try_every_row_and_column
-    def block_long_enough(self, index, values, direction):
+    def get_known_blocks(self, index, values, direction):
         tiles = self.get_line(direction, index)
-        changes_made = False
         last_tile_filled = False
         value_this_block = 'unknown'
-        for position, tile in enumerate(tiles):
+        blocks = []
+        for position, tile in enumerate(tiles + [empty_tile]):
             if tile.filled:
                 if last_tile_filled:
                     length_contiguous += 1
@@ -388,15 +414,176 @@ class NonogramSolver(NonogramGrid):
                 if tile.decided[direction]:
                     value_this_block = tile.possible_values[direction][0]
                 last_tile_filled = True
-            if not tile.filled:
+            else:
                 if value_this_block != 'unknown':
-                    if value_this_block == length_contiguous:
-                        # we've found the whole thing so let's put empties either end
-                        changes_made += tile.set_only_option(empty)
-                        if start_contiguous > 0:
-                            changes_made += tiles[start_contiguous - 1].set_only_option(empty)
-                    for tile_to_check in tiles[start_contiguous:start_contiguous+length_contiguous]:
-                        changes_made += tile_to_check.set_only_option(value_this_block, direction)
-                value_this_block = 'unknown'
+                    blocks.append((start_contiguous, length_contiguous, value_this_block))
+                    value_this_block = 'unknown'
                 last_tile_filled = False
+        return blocks
+
+    @try_every_row_and_column
+    def block_long_enough(self, index, values, direction):
+        tiles = self.get_line(direction, index)
+        changes_made = False
+        blocks = self.get_known_blocks(index, values, direction)
+        for start, length, value in blocks:
+            if value == length:
+                # we've found the whole thing so let's put empties either end
+                after = start + length
+                if after < self.size[direction]:
+                    changes_made += tiles[after].set_only_option(empty)
+                if start > 0:
+                    changes_made += tiles[start - 1].set_only_option(empty)
+                    for tile_to_check in tiles[start:start+length]:
+                        changes_made += tile_to_check.set_only_option(value, direction)
+        return changes_made
+
+    @try_every_row_and_column
+    def split_row_by_known_block(self, index, values, direction):
+        tiles = self.get_line(direction, index)
+        changes_made = False
+        blocks = self.get_known_blocks(index, values, direction)
+
+        for start, length, value in blocks:
+            if values.count(value) == 1:
+                where_split = values.index(value)
+                # Definitely know which other blocks have to be before and after this
+                values_before = values[:where_split]
+                values_after = values[where_split+1:]
+                tiles_before = tiles[:start]
+                tiles_after = tiles[start+length:]
+                changes_made += self.fill_fully(values_before, direction, tiles_before)
+                changes_made += self.fill_fully(values_after, direction, tiles_after)
+                changes_made += self.rule_out_values_based_on_already_used_up_partial(
+                    index, values_before, direction, tiles_before
+                )
+                changes_made += self.rule_out_values_based_on_already_used_up_partial(
+                    index, values_after, direction, tiles_after
+                )
+        return changes_made
+
+    @try_every_row_and_column
+    def too_far_from_known_block_repeated_values(self, index, values, direction):
+        tiles = self.get_line(direction, index)
+        changes_made = False
+        known_by_value = defaultdict(list)
+
+        for position, tile in enumerate(tiles):
+            if tile.filled and tile.decided[direction]:
+                known_by_value[tile.possible_values[direction][0]].append(position)
+
+        for value in set(values):
+            if values.count(value) == 1:
+                # other rules take care of this pretty well
+                pass
+            elif values.count(value) > 2:
+                # TODO harder than the twice repeated case
+                pass
+            else:
+                # appears exactly twice
+                relevant_positions = known_by_value[value]
+                relevant_positions.sort()
+                if len(relevant_positions) < 2:
+                    continue
+                lowest = relevant_positions[0]
+                highest = relevant_positions[-1]
+                if relevant_positions[-1] - relevant_positions[0] >= value:
+                    # we know both instances of this value are implicitly (partially) placed
+                    in_left = [lowest]
+                    in_right = [highest]
+                    for position in relevant_positions[1:-1]:
+                        if position - lowest >= value and highest - position >= value:
+                            raise NonogramImpossible(
+                                "The blocks with value {} are {} (and maybe others) which makes no sense"
+                                .format(value, relevant_positions)
+                            )
+                        elif position - lowest < value and highest - position < value:
+                            # don't know which block this is in of the two
+                            continue
+                        elif position - lowest < value:
+                            in_left.append(position)
+                        elif highest - position < value:
+                            in_right.append(position)
+                        else:
+                            raise Exception("Some fishy logic going on here")
+
+                    in_left.sort()
+                    in_right.sort()
+
+                    if direction == 'row' and index == 13:
+                        print in_left
+                        print in_right
+
+                    for positions in [in_left, in_right]:
+                        if len(positions) > value:
+                            raise Exception("some fishy logic going on here")
+                        if len(positions) < positions[-1] - positions[0]:
+                            # Some values are missing from this list
+                            changes_made = True
+                            for position in xrange(positions[0], positions[-1]):
+                                tiles[position].set_only_option(value, direction)
+
+                    for position, tile in enumerate(tiles):
+                        if (
+                            (abs(position - in_left[0]) >= value or abs(position - in_left[-1]) >= value) and
+                            (abs(position - in_right[0]) >= value or abs(position - in_right[-1]) >= value)
+                        ):
+                            # it can't be in either group
+                            if value in tile.possible_values[direction]:
+                                tile.remove_option(value, direction)
+                                changes_made = True
+        return changes_made
+
+    @try_every_row_and_column
+    def speculative_place_end_of_row(self, index, value, direction):
+        # not convinced this has a lot of value
+        # however we can sometimes reach otherwise hard to spot contradictions by speculatively putting something
+        # at the end of a row and reaching a contradiction
+        # at which point we know that end of row slot is definitely unfilled
+        pass
+
+    # TODO some improvement when filling in from the start or end. currently stop at the first empty tile but
+    # other known not filled tiles in the row might rule out that as well
+
+    @try_every_row_and_column
+    def eliminate_wrong_side(self, index, values, direction):
+        tiles = self.get_line(direction, index)
+        changes_made = False
+        unique_values = set(values)
+
+        fixed_points_by_value = defaultdict(list)
+
+        for position, tile in enumerate(tiles):
+            if tile.filled and tile.decided[direction]:
+                # Then we have a fixed point to use
+                fixed_points_by_value[tile.possible_values[direction][0]].append(position)
+
+        for value, fixed_points in fixed_points_by_value.iteritems():
+            fixed_points.sort()
+            first_appearance = values.index(value)
+            # why is there no rindex !?
+            last_appearance = first_appearance
+            for position, possible_match in enumerate(values):
+                if possible_match == value:
+                    last_appearance = position
+
+            for eliminate in unique_values:
+                if value == eliminate:
+                    continue
+                if eliminate not in values[first_appearance:]:
+                    # This value does not appear after the fixed point value
+                    # so remove it as an option from all those tiles
+                    for tile in tiles[fixed_points[0]:]:
+                        if eliminate in tile.possible_values[direction]:
+                            changes_made = True
+                            tile.remove_option(eliminate, direction)
+
+                if eliminate not in values[:last_appearance]:
+                    # This value does not appear before the fixed point value
+                    # so remove it as an option from all those tiles
+                    for tile in tiles[:fixed_points[-1]]:
+                        if eliminate in tile.possible_values[direction]:
+                            changes_made = True
+                            tile.remove_option(eliminate, direction)
+
         return changes_made
