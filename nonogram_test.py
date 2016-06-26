982099b8a329ebb51daab8fb956b87f89cf1d3f6
rwgeaston <rwgeaston@gmail.com>
2016-06-26 11:19:53 +0100

    use new tally function to see which are most useful rules
    {'block_long_enough': 521,
    'cross_out_too_far_from_any_block': 35,
    'cross_out_too_far_from_known_value': 1530,
    'eliminate_wrong_side': 409,
    'fill_block_if_it_touches_edge': 407,
    'fill_from_edge': 389,
    'fill_fully_entire_line': 15,
    'fill_middle': 2811,
    'got_enough_filled_or_not_filled': 335,
    'next_to_known_empty': 1061,
    'remove_options_if_other_pieces_before_it': 887,
    'rule_out_values_based_on_already_used_up': 576,
    'rule_out_values_too_small_for_this_block': 783,
    'split_row_by_known_block': 473,
    'too_far_from_known_block_repeated_values': 273}

--- nonogram_test.py
+++ nonogram_test.py
@@ -1,20 +1,22 @@
 import os
+from pprint import pprint
+from collections import defaultdict
 
-from nonograms import nonograms_input_reader
-from nonogram_solver import NonogramSolver
+from nonograms import nonograms_input_reader, empty
+from tally_nonogram_rules import tally_nonogram_rules_used
 
 local_folder = os.path.dirname(os.path.realpath(__file__))
-data = nonograms_input_reader(os.path.join(local_folder, "nonograms_data4.txt"))
-solver = NonogramSolver(data['rows'], data['columns'])
-print unicode(solver)
+data = nonograms_input_reader(os.path.join(local_folder, "nonograms_data6.txt"))
+rows_sum = sum((sum(row) for row in data['rows']))
+columns_sum = sum((sum(column) for column in data['columns']))
+if rows_sum != columns_sum:
+    print len(data['rows']), rows_sum
+    print len(data['columns']), columns_sum
+    raise Exception("This doesn't add up")
 
-while True:
-    outcome = solver.try_all_rules()
-    if not outcome:
-        break
-    print outcome
-    #print solver.__unicode__('row')
-    #print solver.__unicode__('column')
+tallies = defaultdict(int)
+for i in range(20):
+    solver, tallies = tally_nonogram_rules_used(data['rows'], data['columns'], tallies)
 
-print "final grid:"
-print unicode(solver)
+    print unicode(solver)
+    pprint(dict(tallies))
